42cc4fa9927a4fddbe9c330f85ebbaad5e6db841
buzhibujue <1752862657@qq.com>
2023-02-25 02:14:28 +0800

    Rewrite output of measure.py

--- sensor/measure.py
+++ sensor/measure.py
@@ -1,12 +1,13 @@
 import threading
-import RTrobot_ENS210
 import sys
 import time
+from datetime import datetime
 import RPi.GPIO as GPIO
-import RTrobot_ENS160
-import RTrobot_LTR390UV
+from ENS160.RTrobot_ENS160 import * 
+from ENS210.RTrobot_ENS210 import * 
+from LTR390UV.RTrobot_LTR390UV import * 
 
-ens210 = RTrobot_ENS210.RTrobot_ENS210()
+ens210 = RTrobot_ENS210()
 buf210 = ens210.ENS210_Init()
 
 #温度和湿度
@@ -21,26 +22,23 @@ air_Data1 = 0
 air_Data2 = 0
 air_Data3 = 0
 
-ens160 = RTrobot_ENS160.RTrobot_ENS160(mode=ENS160_MODE_I2C, spi_cs=8)
+ens160 = RTrobot_ENS160(mode=ENS160_MODE_I2C, spi_cs=8)
 buf160 = ens160.ENS160_Init()
 
 #空气质量
 light_Data1 = 0
 light_Data2 = 0
 
-ltr390 = RTrobot_LTR390UV.RTrobot_LTR390UV()
+ltr390 = RTrobot_LTR390UV()
 buf390 = ltr390.LTR390UV_Init()
 
 #每600秒进行一次repeat操作
 def createTimer():
-    t = threading.Timer(600, repeat)
+    t = threading.Timer(5, repeat)
     t.start()
 
 def repeat():
-    printData1()
-    printData2()
-    printData3()
-    file_handle = open('../data.txt', 'a')
+    file_handle = open('./data.txt', 'a')
     file_handle.write(str(temperature1) + " " + str(humidity1) + " ")
     file_handle.write(str(air_Data1) + " " + str(air_Data2) + " " + str(air_Data3) + " ")
     file_handle.write(str(light_Data1) + " " + str(light_Data2) + "\n")
@@ -48,6 +46,11 @@ def repeat():
     file_handle.close()
 
     createTimer()
+
+def printTime():
+    tim = datetime.now()
+    print(tim.strftime("%H:%M:%S"),'\t',end='')
+
 
 def printData1():
     t_value, h_value = ens210.ENS210_GetMeasure()
@@ -58,7 +61,7 @@ def printData1():
     temperature1 = format(ens210.ENS210_Convert_Temperature(t_value), '.2f')
     humidity1 = format(ens210.ENS210_Convert_Humidity(h_value), '.2f')
 
-    print(temperature1, humidity1, )
+    print(f"Temp: {temperature1}\tHumi: {humidity1}\t",end='')
 
     time.sleep(1)
 
@@ -70,7 +73,7 @@ def printData2():
     air_Data1 = ens160._data_aqi
     air_Data2 = ens160._data_tvoc
     air_Data3 = ens160._data_eco2
-    print("AQI:%d\tTVOC:%dppb\teCO2:%dppm\r\n" % (ens160._data_aqi, ens160._data_tvoc, ens160._data_eco2))
+    print("AQI: %d\tTVOC: %dppb\teCO2: %dppm\t" % (ens160._data_aqi, ens160._data_tvoc, ens160._data_eco2),end='')
     time.sleep(1)
 
 def printData3():
@@ -80,13 +83,13 @@ def printData3():
         if ltrmode == ltr390.LTR390UV_MODE_ALS:
             als = ltr390.LTR390UV_GetALS()
             lux = ltr390.LTR390UV_GetLUX(als, 1)
-            print("ALS: %d,\tLux: %f\r\n" % (als, lux))
+            print("ALS: %d\tLux: %f\r\n" % (als, lux))
             light_Data1 = als
             light_Data2 = lux
         else:
             uvs = ltr390.LTR390UV_GetUVS()
             uvi = ltr390.LTR390UV_GetUVI(uvs, 1)
-            print("UVS: %d,\tUVI: %f\r\n" % (uvs, uvi))
+            print("UVS: %d\tUVI: %f\r\n" % (uvs, uvi))
             light_Data1 = uvs
             light_Data2 = uvi
     time.sleep(0.5)
@@ -135,6 +138,7 @@ createTimer()
 
 try:
     while True:
+        printTime()
         printData1()
         printData2()
         printData3()
